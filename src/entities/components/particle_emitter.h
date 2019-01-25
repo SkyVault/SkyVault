123ebc6c6fa8133b2c0f1331db029643406fab21
SkyVault <dustinneumann42@gmail.com>
2019-01-25 14:43:38 -0800

    Created particle system emitter file

---
+++ src/entities/components/particle_emitter.h
@@ -0,0 +1,18 @@
+#ifndef SKYVAULT_PARTICLE_EMITTER
+#define SKYVAULT_PARTICLE_EMITTER
+#endif//SKYVAULT_PARTICLE_EMITTER
+
+#include "../component.h"
+
+#include <SFML/Graphics.hpp>
+
+struct Particle {
+
+
+};
+
+struct ParticleEmitter {
+
+
+
+};
